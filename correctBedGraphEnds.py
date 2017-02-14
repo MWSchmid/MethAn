5eb6e57045d307d4765ee9bb3c118404d2277417
MWSchmid <schmid.m@access.uzh.ch>
2017-02-14 11:30:30 +0100

    added a script for correcting ends in a bedGraph

---
+++ correctBedGraphEnds.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""correctBedGraphEnds.py
+
+This script can be used to correct the ends in a bedGraph if they are falling off the chromosome.
+
+"""
+
+__author__ = "Marc W Schmid"
+__version__ = "0"
+
+import argparse
+import sys
+import logging
+import textwrap
+import operator
+logging.basicConfig(format="=== %(levelname)s === %(asctime)s === %(message)s", level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
+
+class methBin():
+    def __init__(self, chrom, start, end):
+        self.chrom = chrom
+        self.start = start
+        self.end = end
+        self.anyContext = ["CpG", "CG", "CHG", "CHH"]
+        self.count = dict([(x, 0) for x in self.anyContext])
+        self.meth = dict([(x, 0) for x in self.anyContext])
+    
+    def __str__(self):
+        outLines = []
+        for ctxt in self.anyContext:
+            if self.count[ctxt] > 0:
+                aveMeth = self.meth[ctxt]/self.count[ctxt]
+            else:
+                aveMeth = "NA"
+            outLines.append('\t'.join([self.chrom, str(self.start), str(self.end), ctxt, str(aveMeth)]))
+        out = '\n'.join(outLines)
+        return out
+    
+    def hasValues(self):
+        for ctxt in self.anyContext:
+            if self.count[ctxt] > 0:
+                return True
+        return False
+    
+    def tryToAddPosition(self, chrom, position, ctxt, percMeth):
+        if (chrom != self.chrom) or (position < self.start) or (position >= self.end): # half open
+            return False
+        self.count[ctxt] += 1
+        self.meth[ctxt] += percMeth
+        return True
+
+def correctBedGraph(bedGraphFile, chromSizesFile):
+    chromSizes = {}
+    with open(chromSizesFile, 'rb') as infile:
+        for line in infile:
+            (chrom, size) = line.decode("ascii").rstrip('\n').split('\t')
+            chromSizes[chrom] = int(size)
+    with open(bedGraphFile, "rb") as infile:
+        for line in infile:
+            (chrom, start, end, value) = line.decode("ascii").rstrip('\n').split('\t')
+            if int(end) > chromSizes[chrom]:
+                end = str(chromSizes[chrom])
+            sys.stdout.write('\t'.join([chrom, start, end, value])+'\n')
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        prog="correctBedGraphEnds.py",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent("""\
+            This script can be used to correct the ends in
+            a bedGraph if they are falling off the chromosome.
+            
+            LC_COLLATE=C
+            correctBedGraphEnds.py $BEDGRAPH $CHROMSIZES | sort -k1,1 -k2,2n > $NEWBEDGRAPH
+            
+            The results are printed to stdout.
+            """))
+
+    parser.add_argument("-v", "--version", action="version",
+                        version='%(prog)s {0}'.format(__version__))
+    
+    parser.add_argument("bedGraph", type=str,
+                        help="""
+                        A file with the methylation data.
+                        """)
+
+    parser.add_argument("chromSizes", type=str,
+                        help="""
+                        A file with chrom ids and size (tab-sep).
+                        """)
+
+    args = parser.parse_args()
+    
+    correctBedGraph(args.bedGraph, args.chromSizes)
+
